cdbc1eac7d30742ea70833cab1b86658de555839
olegKa <kalinin_oleg@list.ru>
2017-05-03 09:47:04 +0500

    Release 0.1.4

---
+++ OKTextFieldView/Classes/OKPhoneTextFieldView.h
@@ -0,0 +1,13 @@
+//
+//  OKPhoneTextFieldView.h
+//  Pods
+//
+//  Created by OLEG KALININ on 03.05.17.
+//
+//
+
+#import <OKTextFieldView/OKTextFieldView.h>
+
+@interface OKPhoneTextFieldView : OKTextFieldView
+
+@end
---
+++ OKTextFieldView/Classes/OKPhoneTextFieldView.m
@@ -0,0 +1,21 @@
+//
+//  OKPhoneTextFieldView.m
+//  Pods
+//
+//  Created by OLEG KALININ on 03.05.17.
+//
+//
+
+#import "OKPhoneTextFieldView.h"
+
+@implementation OKPhoneTextFieldView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
